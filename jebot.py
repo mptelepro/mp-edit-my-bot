b5ed9b77b85cb255a9ffef03d8740421e2446be9
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-24 15:06:15 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -4,12 +4,6 @@ from pyrogram import Client, filters
 from telegraph import upload_file
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-Jebot = Client(
-   "Telegraph Uploader",
-   api_id=Config.APP_ID,
-   api_hash=Config.API_HASH,
-   bot_token=Config.TG_BOT_TOKEN,
-)
 
 @Jebot.on_message(filters.command("start"))
 async def start(client, message):
