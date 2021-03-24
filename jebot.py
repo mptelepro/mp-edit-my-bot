f8435b974a4bf722e8451c42271e0dc947f193fc
mptelepro <72145569+mptelepro@users.noreply.github.com>
2021-03-24 15:09:53 +0530

    Update jebot.py

--- jebot.py
+++ jebot.py
@@ -2,6 +2,7 @@ import os
 import logging
 from pyrogram import Client, filters
 from telegraph import upload_file
+from config import Config
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 Jebot = Client(
